3779a9813d803595c718f80ef6a7877e538b1517
Hanapibagas <hanafibagas4@gmail.com>
2023-01-25 17:42:18 +0800

    finish

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Post;
+
+class PostRepository
+{
+    protected $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+    public function getAll()
+    {
+        return $this->post->get();
+    }
+
+    public function save($data)
+    {
+        $post = new $this->post;
+
+        $post->title = $data['title'];
+        $post->deskripsi = $data['deskripsi'];
+
+        $post->save();
+
+        return $post->fresh();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContactController::class, 'index']);
+Route::get('/contact/{id}', [ContactController::class, 'show']);
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/post/create', [PostController::class, 'store']);
